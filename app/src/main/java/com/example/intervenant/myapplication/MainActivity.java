4344fb4367da524a33fc28ea0dd3efcd38dbdd80
GIGONNET Jean-charles <jgigonnet@mac-ck92400z20h.intra.gobelins.fr>
2016-04-21 11:00:58 +0200

    #7 - ViewPager and Fragment ok ☝🏼

--- app/src/main/java/com/example/intervenant/myapplication/MainActivity.java
+++ app/src/main/java/com/example/intervenant/myapplication/MainActivity.java
@@ -1,5 +1,10 @@
 package com.example.intervenant.myapplication;
 
+import android.net.Uri;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,8 +12,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.example.intervenant.myapplication.com.example.intervenant.core.Frangment;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, Frangment.OnFragmentInteractionListener {
 
     ViewPager viewPager;
 
@@ -17,12 +24,44 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        viewPager = findViewById(R.id.pager);
+        viewPager = (ViewPager) findViewById(R.id.pager);
+        viewPager.setAdapter(new ListPagerAdapter(getSupportFragmentManager()));
+
+        final TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
     }
 
     @Override
     public void onClick(View view) {
 
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+
+    public class ListPagerAdapter extends FragmentPagerAdapter {
+
+        public ListPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return Frangment.newInstance(position);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return position== 0 ? "Products" : "Favorites";
+        }
+
+        @Override
+        public int getCount() {
+            return 2;
+        }
     }
 
 
