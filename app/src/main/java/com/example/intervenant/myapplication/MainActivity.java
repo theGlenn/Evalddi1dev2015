9216d84bab4f607f8f211ac87dfd1371f5ca3622
Theo Baes <baes.theo@gmail.com>
2016-04-21 10:45:58 +0200

    Fix #1 #7

--- app/src/main/java/com/example/intervenant/myapplication/MainActivity.java
+++ app/src/main/java/com/example/intervenant/myapplication/MainActivity.java
@@ -1,11 +1,18 @@
 package com.example.intervenant.myapplication;
 
+import android.content.Intent;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import com.example.intervenant.myapplication.fragments.ProductListViewFragment;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
@@ -17,12 +24,49 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        viewPager = findViewById(R.id.pager);
+        viewPager = (ViewPager) findViewById(R.id.pager);
+        viewPager.setAdapter(new ListPagerAdapter(getSupportFragmentManager()));
+
+        final TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
     }
 
     @Override
     public void onClick(View view) {
 
+    }
+
+    /*@Override
+    public void onFragmentListInteraction(Product product) {
+        Intent detailIntent = new Intent(this, DetailActivity.class);
+        detailIntent.putExtra("name", product.name);
+        detailIntent.putExtra("image", product.image);
+        detailIntent.putExtra("price", product.price);
+        detailIntent.putExtra("info", product.info);
+
+        startActivity(detailIntent);
+    }*/
+
+    public class ListPagerAdapter extends FragmentPagerAdapter {
+
+        public ListPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return ProductListViewFragment.newInstance(position);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return position== 0 ? "Produits" : "Panier";
+        }
+
+        @Override
+        public int getCount() {
+            return 2;
+        }
     }
 
 
--- app/src/main/java/com/example/intervenant/myapplication/com/example/intervenant/core/ProductProvider.java
+++ app/src/main/java/com/example/intervenant/myapplication/com/example/intervenant/core/ProductProvider.java
@@ -1,6 +1,19 @@
 package com.example.intervenant.myapplication.com.example.intervenant.core;
 
+import android.content.Context;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.intervenant.myapplication.Product;
 import com.example.intervenant.myapplication.R;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
@@ -9,4 +22,56 @@ import java.util.ArrayList;
  */
 public class ProductProvider {
 
+    public static  ArrayList<Product> provideFromBasket(){
+        ArrayList<Product> list = new ArrayList<>();
+        list.add(new Product("apple", "http://vignette4.wikia.nocookie.net/farmville2/images/3/34/Lime.png/revision/latest?cb=20121011124753", "2", "foo"));
+        list.add(new Product("grapefruit", "http://vignette4.wikia.nocookie.net/farmville2/images/3/34/Lime.png/revision/latest?cb=20121011124753", "2", "bar"));
+
+        return list;
+    }
+
+    public static void provideFromServer(Context context, final VolleyCallback callback){
+        RequestQueue queue = Volley.newRequestQueue(context);
+        String url = "https://api.myjson.com/bins/27dd6";
+        final ArrayList<Product> list = new ArrayList<Product>();
+
+        JsonObjectRequest jsObjRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONArray jArray = (JSONArray)response.getJSONArray("data");
+
+                            for (int i = 0; i < jArray.length(); i++){
+                                JSONObject json_data = jArray.getJSONObject(i);
+
+                                list.add(new Product(
+                                        json_data.getString("name"),
+                                        json_data.getString("image"),
+                                        json_data.getString("price"),
+                                        json_data.getString("info")));
+
+                                callback.onSuccess(list);
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO Auto-generated method stub
+
+                    }
+                });
+
+        // Access the RequestQueue through your singleton class.
+        queue.add(jsObjRequest);
+    }
+
+    public interface VolleyCallback{
+        void onSuccess(ArrayList<Product> string);
+    }
 }
