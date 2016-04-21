b7bd78ffd03f9f2ecbf6b62bfd4820213ab63400
Thomas Boulongne <thomasboulongne@hotmail.com>
2016-04-21 11:00:59 +0200

    #7 1 Use ViewPager to show fragments on the MainActivity

--- app/src/main/java/com/example/intervenant/myapplication/MainActivity.java
+++ app/src/main/java/com/example/intervenant/myapplication/MainActivity.java
@@ -1,5 +1,10 @@
 package com.example.intervenant.myapplication;
 
+import android.net.Uri;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,8 +12,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.example.intervenant.myapplication.com.example.intervenant.core.Fragment1;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, Fragment1.OnFragmentInteractionListener {
 
     ViewPager viewPager;
 
@@ -18,6 +25,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         viewPager = (ViewPager) findViewById(R.id.pager);
+        viewPager.setAdapter(new PagerAdapter(getSupportFragmentManager()));
+
+        final TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
     }
 
     @Override
@@ -25,5 +36,33 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     }
 
+    @Override
+    public void onFragmentInteraction(Uri uri) {
 
+    }
+
+    public static class PagerAdapter extends FragmentPagerAdapter {
+
+        private static int NUM_ITEMS = 2;
+
+        public PagerAdapter(FragmentManager fragmentManager) {
+            super(fragmentManager);
+        }
+
+        @Override
+        public int getCount() {
+            return NUM_ITEMS;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return Fragment1.newInstance(position);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return position== 0 ? "Something" : "Some other thing";
+        }
+
+    }
 }
