5bc6531fb0ca4c67e5d70e31afd5d876db1a4de6
XING Lei <lei.xing@hotmail.fr>
2016-04-21 11:00:37 +0200

    7 - 1 Use ViewPager to show fragments on the MainActivity

--- app/src/main/java/com/example/intervenant/myapplication/MainActivity.java
+++ app/src/main/java/com/example/intervenant/myapplication/MainActivity.java
@@ -1,5 +1,10 @@
 package com.example.intervenant.myapplication;
 
+import android.net.Uri;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,8 +12,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.example.intervenant.myapplication.com.example.intervenant.core.ViewFragment;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener,ViewFragment.OnFragmentInteractionListener {
 
     ViewPager viewPager;
 
@@ -17,13 +24,45 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        viewPager = findViewById(R.id.pager);
+        viewPager = (ViewPager) findViewById(R.id.pager);
+        viewPager.setAdapter(new GridPagerAdapter(getSupportFragmentManager()));
+
+        final TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
     }
 
     @Override
     public void onClick(View view) {
 
     }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+
+    public class GridPagerAdapter extends FragmentPagerAdapter {
+
+        public GridPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return ViewFragment.newInstance(position);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return position== 0 ? "All Products" : "Cart";
+        }
+
+        @Override
+        public int getCount() {
+            return 2;
+        }
+    }
+
 
 
 }
