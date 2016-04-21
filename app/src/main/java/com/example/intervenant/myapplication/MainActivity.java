51a703cce3efc0c4155ec97488fb445bdea5f731
Intervenant ponctuel sur toutes les formations <intervenant@self.intra.gobelins.fr>
2016-04-21 09:32:03 +0200

    fruit-app-base

--- app/src/main/java/com/example/intervenant/myapplication/MainActivity.java
+++ app/src/main/java/com/example/intervenant/myapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.intervenant.myapplication;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,33 +10,20 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    ViewPager viewPager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView textView = (TextView) findViewById(R.id.hello);
-        textView.setText(R.string.text_test);
 
-        Button goList = (Button)findViewById(R.id.goListButton);
-        goList.setOnClickListener(this);
-        goList.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                goListToListActivity();
-            }
-        });
-
-
-
-
+        viewPager = findViewById(R.id.pager);
     }
 
     @Override
     public void onClick(View view) {
-        goListToListActivity();
-    }
-
-    public void goListToListActivity(){
 
     }
+
+
 }
---
+++ app/src/main/java/com/example/intervenant/myapplication/com/example/intervenant/core/ProductProvider.java
@@ -0,0 +1,12 @@
+package com.example.intervenant.myapplication.com.example.intervenant.core;
+
+import com.example.intervenant.myapplication.R;
+
+import java.util.ArrayList;
+
+/**
+ * Created by intervenant on 18/04/16.
+ */
+public class ProductProvider {
+
+}
